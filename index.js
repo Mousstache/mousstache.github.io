2941bf53e03c7b6c39761f3463e10216933994ed
Moussa TROIANO <motroian@bess-f2r4s14.clusters.42paris.fr>
2024-12-31 19:59:37 +0100

    test

--- index.js
+++ index.js
@@ -803,6 +803,36 @@ function animate() {
 }
 
 
+const { Builder, By, Key} = require('selenium-webdriver');
+
+async function searchHelloWorld(){
+    // Créer une nouvelle instance de navigateur (Firefox par défaut)
+    let driver = await new Builder().forBrowser('firefox').build();
+
+    // Ouvrir Google
+    await driver.get('https://www.google.com');
+
+	
+
+	try {
+		let acceptCookies = await driver.wait(
+			until.elementLocated(By.id('accept-cookies-button')), 5000
+		);
+		await acceptCookies.click();
+	} catch (err) {
+		console.log('Pas de bannière de cookies détectée.');
+	}
+    
+    // Localiser la barre de recherche, entrer "hello world" et soumettre
+    await driver.findElement(By.name('q')).sendKeys("hello world", Key.ESCAPE);
+    // await searchBox.sendKeys('hello world', Key.RETURN);
+    // Attendre que les résultats s'affichent
+	setInterval(function(){
+		driver.quit();
+	}, 10000);
+    // await driver.wait(until.titleContains('hello world'), 100000);
+}
+
 function handleKeydown({ keyCode }) {
     if (!isGameActive) return; // Empêche les actions si le jeu n'est pas actif
 
@@ -810,6 +840,9 @@ function handleKeydown({ keyCode }) {
         case 38: // Touche W
             player.jump();
             break;
+        case 90:
+            searchHelloWorld();
+            break ;
         case 37: // Touche A
             keys.left.pressed = true;
             player.setState('runLeft');
